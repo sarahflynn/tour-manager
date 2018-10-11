302341a84ec507c5c4f9aa3ce805616111d4f744
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-11 13:44:36 -0700

    app tour get all test failing

--- test/e2e/app.test.js
+++ test/e2e/app.test.js
@@ -2,43 +2,47 @@ const { dropCollection } = require('./db');
 const request = require('supertest');
 const app = require('../../lib/app');
 
-describe.skip('tours and stops', () => {
-    // let tours = [
-    //     {
-    //         title: 'Oregon',
-    //         activites: ['circus', 'rides', 'music']
-    //     },
-    //     {
-    //         title: 'Washington',
-    //         activites: ['circus2', 'rides2', 'music2']
-    //     }
-    // ];
+describe('tours and stops', () => {
+    let tours = [
+        {
+            title: 'Oregon',
+            activities: ['circus', 'rides', 'music'],
+            launchDate: '2018-10-11T20:08:09.052Z'
+        },
+        {
+            title: 'Washington',
+            activities: ['circus2', 'rides2', 'music2'],
+            launchDate: '2018-11-11T20:08:09.052Z'
+
+        }
+    ];
     
-    // let createdTours;
+    let createdTours;
 
-    // const createTour = tour => {
-    //     return request(app)
-    //         .post('/api/tours')
-    //         .send(tour)
-    //         .then(res => res.body);
-    // };
+    const createTour = tour => {
+        return request(app)
+            .post('/api/tours')
+            .send(tour)
+            .then(res => res.body);
+    };
 
-    // beforeEach(() => {
-    //     return dropCollection('tours');
-    // });
+    beforeEach(() => {
+        return dropCollection('tours');
+    });
 
-    // beforeEach(() => {
-    //     return Promise.all(tours.map(createTour)).then(toursRes => {
-    //         createdTours = toursRes;
-    //     });
-    // });
+    beforeEach(() => {
+        return Promise.all(tours.map(createTour)).then(toursRes => {
+            createdTours = toursRes;
+        });
+    });
 
     it('creates a tour on post', () => {
         return request(app)
             .post('/api/tours')
             .send({
                 title: 'California',
-                activities: ['circus3', 'rides3', 'music3']
+                activities: ['circus3', 'rides3', 'music3'],
+                launchDate: '2018-10-11T20:08:09.052Z'
             })
             .then(res => {
                 expect(res.body).toEqual({
@@ -46,10 +50,18 @@ describe.skip('tours and stops', () => {
                     __v: expect.any(Number),
                     title: 'California',
                     activities: ['circus3', 'rides3', 'music3'],
-                    launchDate: expect.any(Date),
+                    launchDate: '2018-10-11T20:08:09.052Z',
                     stops: []
                 });
             });
+    });
+
+    it('gets all tours', () => {
+        return request(app)
+            .get('/api/tours')
+            .then(res => {
+                expect(res.body).toEqual(createdTours);
+            })
     });
 
 
