1bb27db13a3bdd2d633392ac607af29ed5a64f8f
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-10 20:21:39 -0700

    error fixes

--- test/unit/getWeather.test.js
+++ test/unit/getWeather.test.js
@@ -1,18 +1,18 @@
-const getWeather = require('../../lib/util/getWeather');
+const { getWeather } = require('../../lib/util/getWeather');
 
 describe('get weather middleware', () => {
     it('calls next with no error when provided with a valid zip', () => {
-        const weatherMiddleware = getWeather();
         const req = 97205;
-
+        
         let called = false;
         let error;
+        
         const next = err => {
             called = true;
             error = err;
         };
 
-        weatherMiddleware(req, next);
+        getWeather(req, null, next);
         expect(called).toBeTruthy();
         expect(error).toBeUndefined();
     })
